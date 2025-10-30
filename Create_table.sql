799d6d5ed570e6359fd3112ff0ef9d4b6f20e9df
ajithkumar3103k-web <ajithkumar3103.k@gmail.com>
2025-10-30 22:56:22 +0530

    Create&insert

---
+++ Create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE SHIPMENTS (
+    Shipment_ID INT PRIMARY KEY,
+    Pickup_Date DATE,
+    Delivery_Date DATE,
+    Origin_Location VARCHAR(100),
+    Destination_Location VARCHAR(100)
+);
